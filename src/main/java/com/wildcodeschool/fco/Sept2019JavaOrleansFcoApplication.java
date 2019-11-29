954329add422629a3d73dc5a3558e7945f4ecc4f
Ouchrif Ayoub <ouchrif.ayoub@gmail.com>
2019-11-29 21:22:45 +0100

    init project

---
+++ src/main/java/com/wildcodeschool/fco/Sept2019JavaOrleansFcoApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.fco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sept2019JavaOrleansFcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sept2019JavaOrleansFcoApplication.class, args);
+	}
+
+}
