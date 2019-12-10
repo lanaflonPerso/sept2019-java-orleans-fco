80bf2fe9a1b4f4ee767bccc0439de290aa8f76da
Quentin Liger <quentinlgr45160@gmail.com>
2019-12-10 19:08:39 +0100

    fixs

--- src/main/java/com/wildcodeschool/fco/controller/ContactController.java
+++ src/main/java/com/wildcodeschool/fco/controller/ContactController.java
@@ -2,6 +2,7 @@ package com.wildcodeschool.fco.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,7 @@ public class ContactController {
 	}
 	
 	@PostMapping("/contact")
-	public String getMessage(@ModelAttribute Message message) {
+	public String getMessage(@ModelAttribute Message message, Model model) {
 		messageRepository.save(message);
 		return "contact";
 	}
