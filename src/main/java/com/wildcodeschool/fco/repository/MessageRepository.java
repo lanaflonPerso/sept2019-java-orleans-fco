914cc055c7a50ecb31ecd0ce014cf739e7265c72
Quentin Liger <quentinlgr45160@gmail.com>
2019-12-10 17:24:21 +0100

    add controller

---
+++ src/main/java/com/wildcodeschool/fco/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.wildcodeschool.fco.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wildcodeschool.fco.entity.Message;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+}
