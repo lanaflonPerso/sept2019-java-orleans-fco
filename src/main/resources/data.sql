a6ffb499fcc9befc7f01faedd3a032805d17fb29
Quentin Liger <quentinlgr45160@gmail.com>
2019-12-18 10:56:40 +0100

    edit data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,13 @@
-
-INSERT INTO sponsor VALUES (60,'nike',1,'https://www.nike.com/fr/','/img/photoSponsor/nike.png'),(61,'qatar',2,'https://www.qatarairways.com/fr-dz/homepage.html','/img/photoSponsor/qta.png'),(62,'ooredoo',3,'http://www.ooredoo.tn/','/img/photoSponsor/ooredoo-alt.png'),(63,'accor live limitless',4,'https://all.accor.com/geoloc/selectdisplayzone/index.gb.shtml','/img/photoSponsor/all.png'),(64,'QNB',5,'https://www.qnb.com/sites/qnb/qnbglobal/page/en/enqnbglobalnetwork.html','/img/photoSponsor/qnb-secondary.png'),(65,'rouwanda',7,'https://www.visitrwanda.com/','/img/photoSponsor/visit-rwanda.png'),(66,'soir DE match',6,'www.soirdematch.fr','/img/photoSponsor/sponsoFCO.png'),(67,'renault',8,'https://www.renault.fr/','/img/photoSponsor/renault.png'),(68,'deliveroo',9,'https://deliveroo.fr/en/','/img/photoSponsor/deliveroo.png');
+INSERT INTO sponsor VALUES
+(1,'Umbro',1,'https://www.umbro.fr/','/img/photoSponsor/umbro.png'),
+(2,'St Jean de la Ruelle',2,'http://www.ville-saintjeandelaruelle.fr/','/img/photoSponsor/logo-st-jean.png'),
+(3,'ooredoo',3,'http://www.ooredoo.tn/','/img/photoSponsor/ooredoo-alt.png'),
+(4,'accor live limitless',4,'https://all.accor.com/geoloc/selectdisplayzone/index.gb.shtml','/img/photoSponsor/all.png'),
+(5,'QNB',5,'https://www.qnb.com/sites/qnb/qnbglobal/page/en/enqnbglobalnetwork.html','/img/photoSponsor/qnb-secondary.png'),
+(6,'rouwanda',7,'https://www.visitrwanda.com/','/img/photoSponsor/visit-rwanda.png'),
+(7,'soir DE match',6,'www.soirdematch.fr','/img/photoSponsor/sponsoFCO.png'),
+(8,'renault',8,'https://www.renault.fr/','/img/photoSponsor/renault.png'),
+(9,'deliveroo',9,'https://deliveroo.fr/en/','/img/photoSponsor/deliveroo.png');
 
 INSERT INTO encounter VALUES (1, "Régionale 1", "Equipe Futsal", 1577228400000, "/img/chateauroux.jpg", "Chateauroux Etoile");
 
